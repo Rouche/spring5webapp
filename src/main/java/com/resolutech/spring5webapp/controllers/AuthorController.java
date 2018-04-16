f0954d2744c0bc41be792c017c4cc7bce75c4644
Jean-Francois Larouche <jean-francois.larouche@dti.ulaval.ca>
2018-04-16 11:56:48 -0400

    Warnings

--- src/main/java/com/resolutech/spring5webapp/controllers/AuthorController.java
+++ src/main/java/com/resolutech/spring5webapp/controllers/AuthorController.java
@@ -1,7 +1,6 @@
 package com.resolutech.spring5webapp.controllers;
 
 import com.resolutech.spring5webapp.repositories.AuthorRepository;
-import com.resolutech.spring5webapp.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/resolutech/spring5webapp/joke/config/ChuckConfiguration.java
+++ src/main/java/com/resolutech/spring5webapp/joke/config/ChuckConfiguration.java
@@ -1,8 +1,6 @@
 package com.resolutech.spring5webapp.joke.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 //@Configuration
 public class ChuckConfiguration {
